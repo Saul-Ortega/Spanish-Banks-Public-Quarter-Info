10ddfdda573f5eebca3fa1ee6aa5b4d065e923f2
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2026-05-20 21:39:48 +0100

    Initialize API

---
+++ api/Spanish-Banks-Quarter-Info-API/src/main/java/org/example/spanishbanksquarterinfoapi/SpanishBanksQuarterInfoApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.spanishbanksquarterinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpanishBanksQuarterInfoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpanishBanksQuarterInfoApiApplication.class, args);
+    }
+
+}
